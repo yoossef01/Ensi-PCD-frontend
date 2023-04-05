adf2bd9ef3f3a9664a98e5dece6a04eb5a8afac8
Chaima-Messadi <messadi.chaimaa@gmail.com>
2023-04-06 00:45:05 +0100

    form

--- src/app/model/vendeur.ts
+++ src/app/model/vendeur.ts
@@ -1,8 +1,10 @@
 export class Vendeur {
+    idTemplate! : number;
     nom! : string;
     prenom! : string;
     adresse! : string;
     tel! : string;
     email! : string;
     password! : string;
+    fax! :string;
 }

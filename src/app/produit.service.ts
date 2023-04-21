07aac2bb81bac723708707e461ac7d663e639d32
Chaima-Messadi <messadi.chaimaa@gmail.com>
2023-04-21 05:29:50 +0100

    comparateur

--- src/app/produit.service.ts
+++ src/app/produit.service.ts
@@ -2,7 +2,7 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { Produit } from './model/produit';
-import { BehaviorSubject } from 'rxjs';
+
 
 
 

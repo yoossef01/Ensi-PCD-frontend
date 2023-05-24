b13e30e1264ef67679774523f5b95fc94c466c60
yoossef01 <105297325+yoossef01@users.noreply.github.com>
2023-05-24 20:43:32 +0100

    updates

--- src/app/components/template1/home1/home1.component.ts
+++ src/app/components/template1/home1/home1.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { TemplateContent } from 'src/app/model/template-content';
 import { Vendeur } from 'src/app/model/vendeur';
+import { TemplateContentService } from 'src/app/template-content.service';
 import { VendeurService } from 'src/app/vendeur.service';
 import Swal from 'sweetalert2';
 import { SweetAlertOptions } from 'sweetalert2';
@@ -17,17 +19,19 @@ interface MySweetAlertOptions extends SweetAlertOptions {
   styleUrls: ['./home1.component.css']
 })
 export class Home1Component implements OnInit {
+  templateContent:TemplateContent=new TemplateContent(0,"","","","",{id:0});
 
-  constructor (private router: Router, private servicevendeur:VendeurService) {}
+  constructor (private router: Router, private servicevendeur:VendeurService,private serviceTemplate:TemplateContentService) {}
 
   ngOnInit(): void {
-
+this.serviceTemplate.getTemplateByVendeur(this.servicevendeur.getIdVendeur()).subscribe(data=>{this.templateContent=data;})
     
   }
 
   navigation() {
     this.router.navigate(['/listeproduitclient1/'+this.servicevendeur.getIdVendeur()]);
   }
+
 
 
 
